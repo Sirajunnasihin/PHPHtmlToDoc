905bb51bed1d3d3420ef56d097825e5713305b75
SirajunNasihin <sirajunnasihin7@gmail.com>
2022-06-12 10:33:51 +0800

    Create PHPHtmlToDoc.php

---
+++ src/Facades/PHPHtmlToDoc.php
@@ -0,0 +1,10 @@
+<?php namespace Sirajunnasihin\PHPHtmlToDoc\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PHPHtmlToDoc extends Facade
+{
+    protected static function getFacadeAccessor() {
+        return 'phphtmltodoc';
+    }
+}
